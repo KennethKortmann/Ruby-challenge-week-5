cfabd806aa57e9db5e570d00a81291f9b36f8b56
KennethKortmann <32777342+KennethKortmann@users.noreply.github.com>
2018-02-13 18:15:01 +0100

    final

--- challenge5.rb
+++ challenge5.rb
@@ -1,40 +1,17 @@
 random_number = rand(100)
 print "Please enter a random number between 0 and 100 > "
-guess = gets.chomp.to_i
+print "You guess #{random_number}"
 
 loop do
-  if random_number == guess
-    puts "Huraaah, you've won!"
+  guess = gets.chomp.to_i
+  if guess < 0
+    puts "Please enter a positve number"
+  elsif guess < random_number
+    puts "Please enter a higher number!"
+  elsif guess > random_number
+    puts "Please enter a lower number"
+  else
+    puts "Congrats, you've won!"
     exit
-  else
-    print "You've guessed #{guess} but that is not your lucky number. Try again!"
-    guess = gets.chomp.to_i
-  end
-
-loop do
-  if random_number == ("-")
-     puts "Please enter positive number"
-     exit
-  else
-    print "You've guessed #{guess} but that is not your lucky number. Try again!"
-    guess = gets.chomp.to_i
-  end
-
-loop do
-  if random_number < guess
-      puts "Your number is too low"
-      exit
-  else
-      print "You've guessed #{guess} but that is not your lucky number. Try again!"
-      guess = gets.chomp.to_i
-  end
-
-loop do
-  if random_number > guess
-     puts "Your number is to high"
-     exit
-  else
-     print "You've guessed #{guess} but that is not your lucky number. Try again!"
-     guess = gets.chomp.to_i
-     exit
-  end
+end
+end
